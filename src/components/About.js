24ab0c5c4c85c3f38e34250cc8a1fbc5ff11c99d
Kai Roberts <kai_roberts@icloud.com>
2023-06-27 20:05:37 +0100

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -28,11 +28,11 @@ const About = () => {
             This is my portfolio website which I created using ReactJS, it's
             purpose is to showcase some of the projects I will undertake in my
             spare time, mainly to keep up with recent technology trends as well
-            as sharpening the skills I gained during University. I plan to
-            include a mixture of things from Front-End Development, Back-End
-            Development, App Development, Networking, Automation and more. As
-            well as any potential freelance websites which I build for clients
-            on the side.
+            as sharpening the skills I gained during University and allow any
+            potential employers to view what I get up to. I plan to include a
+            mixture of things from Front-End Development, Back-End Development,
+            App Development, Networking, Automation and more. As well as any
+            potential freelance websites which I build for clients on the side.
           </p>
         </div>
       </div>
